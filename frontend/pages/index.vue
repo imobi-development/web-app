16fe562edcf5683ea0d4ed0acedbd691a2b8428d
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-10-28 23:33:59 -0300

    feat: chamando api do backend (url fixa)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,9 +2,9 @@
     <h1 class="text-3xl font-bold underline bg-red-500">Olá, Nuxt está funcionando com tailwind 🎉</h1>
     <button @click="navigateTo('/teste/versao1')" class="py-4 bg-blue-500 text-white rounded-md">Teste para ir pra
         rotaaaa</button>
-        <p class="text-3xl text-black">{{ data.message || 'nao tem nada' }}</p>
+        <p class="text-3xl text-black">{{ data.message|| 'nao tem nada' }}</p>
 </template>
 
 <script setup>
-const { data, pending, error } = await useFetch('/api/hello')
+const { data, pending, error } = await useFetch('http://127.0.0.1:8000/api/hello')
 </script>
