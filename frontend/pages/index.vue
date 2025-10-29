2ac87a410114cad9a1c18bb9941382158d8a4fec
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-10-28 21:57:21 -0300

    Initial commit: frontend using Vue + Nuxt.js + TailwindCSS

---
+++ frontend/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="text-3xl font-bold underline bg-red-500">Olá, Nuxt está funcionando com tailwind 🎉</h1>
+    <button @click="navigateTo('/teste/versao1')" class="py-4 bg-blue-500 text-white rounded-md">Teste para ir pra rota</button>
+</template>
