1b8d34fc20727394a994b6684b7b9b0e389e2bdf
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-10-29 10:16:05 -0300

    feat: buscando api base do .env

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,4 +2,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-10-28",
   modules: ["@nuxtjs/tailwindcss"],
   devtools: { enabled: true },
+  runtimeConfig: {
+    // Variáveis privadas que podem ser acessadas somente pelo servidor
+    apiSecret: process.env.NUXT_API_SECRET || 'secret',
+    public: {
+      // Variáveis públicas que podem ser acessadas no frontend
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+    },
+  },
 });
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,9 +2,11 @@
     <h1 class="text-3xl font-bold underline bg-red-500">Olá, Nuxt está funcionando com tailwind 🎉</h1>
     <button @click="navigateTo('/teste/versao1')" class="py-4 bg-blue-500 text-white rounded-md">Teste para ir pra
         rotaaaa</button>
-        <p class="text-3xl text-black">{{ data.message|| 'nao tem nada' }}</p>
+        <p class="text-3xl text-black">{{ data|| 'nao tem nada' }}</p>
 </template>
 
-<script setup>
-const { data, pending, error } = await useFetch('http://127.0.0.1:8000/api/hello')
+<script setup lang="ts">
+const config = useRuntimeConfig()
+console.log("apiBase do .env:::", config.public.apiBase)
+const { data, pending, error } = await useFetch(`${config.public.apiBase}/hello`)
 </script>
